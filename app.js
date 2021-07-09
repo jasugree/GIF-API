1c0cb4aa867f95c400223a3d98a838d53a62c981
Jake Greene <jsgreene208@gmail.com.com>
2021-07-08 21:15:10 -0400

    Pagination now working...

--- app.js
+++ app.js
@@ -20,26 +20,22 @@ searchForm.addEventListener('submit', fetchResults);
 function fetchResults(e){
     e.preventDefault();
    
-    fetch(`${baseURL}?api_key=${key}&q=${searchTerm.value}&limit=8`)
+    fetch(`${baseURL}?api_key=${key}&q=${searchTerm.value}`)
     .then(response => response.json())
     .then(json => displayImage(json));
    
 
     function displayImage(gif){
         let gifLibrary = document.querySelector('.gif-query');
-        
+        console.log(gif);
         while (gifLibrary.firstChild) {
             gifLibrary.removeChild(gifLibrary.firstChild);    
         }        
-        for(i = 0; i < 9; i++){
-        console.log(gif.data[i].images.downsized_large.url);
+        for(i = (pageNumber * 8); i < (pageNumber * 8 + 8); i++){
         let searchedGif = document.createElement('img');
         searchedGif.className += "searched-gif";
         searchedGif.src = gif.data[i].images.downsized_large.url;
         gifLibrary.appendChild(searchedGif);
-        }
-        if(searchedGif == ""){
-            alert('test');
         }
     }
 }
@@ -49,4 +45,31 @@ function showResults() {
     theResults.style.display = "block";  // <-- Set it to block
     var nav = document.getElementById("navigation");
     nav.style.display = "flex";
+}
+
+
+//Pagination
+
+let nextButton = document.querySelector('.next');
+nextButton.addEventListener('click', fetchNextPage);
+
+let prevButton = document.querySelector('.prev');
+prevButton.addEventListener('click', fetchPreviousPage);
+
+let pageNumber = 0;
+
+function fetchNextPage(e){
+    pageNumber++;
+    fetchResults(e);
+    console.log('Page Number:', pageNumber);
+}
+
+function fetchPreviousPage(e){
+    if(pageNumber > 0){
+        pageNumber--;
+    } else {
+        return;
+    }
+    fetchResults(e);
+    console.log('Page:', pageNumber);
 }
